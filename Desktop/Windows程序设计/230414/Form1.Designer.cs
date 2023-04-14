ef5e72d00cba765311f60290c47a31d6f06502ea
ney-messi <130116648+ney-messi@users.noreply.github.com>
2023-04-14 18:34:34 +0800

    video&voice

---
+++ Desktop/Windows程序设计/230414/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿using System;
+
+namespace _230414
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.videoSourcePlayer1 = new AForge.Controls.VideoSourcePlayer();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.videoSourcePlayer1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(33, 70);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(524, 395);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "摄像头";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // videoSourcePlayer1
+            // 
+            this.videoSourcePlayer1.Location = new System.Drawing.Point(19, 59);
+            this.videoSourcePlayer1.Name = "videoSourcePlayer1";
+            this.videoSourcePlayer1.Size = new System.Drawing.Size(499, 278);
+            this.videoSourcePlayer1.TabIndex = 2;
+            this.videoSourcePlayer1.Text = "videoSourcePlayer1";
+            this.videoSourcePlayer1.VideoSource = null;
+            this.videoSourcePlayer1.Click += new System.EventHandler(this.videoSourcePlayer1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(349, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 32);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "扫描";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(370, 347);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(110, 44);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "关闭";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(19, 27);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(266, 26);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(197, 347);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 42);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "拍照";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(19, 345);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 44);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "连接";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.button6);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Location = new System.Drawing.Point(586, 70);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(516, 192);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "麦克风";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(289, 146);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(162, 28);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(318, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 30);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "待检测";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(22, 138);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(186, 37);
+            this.button6.TabIndex = 1;
+            this.button6.Text = "设置麦克风音量";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(22, 59);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(186, 37);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "获取麦克风音量";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textBox2);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.button8);
+            this.groupBox3.Controls.Add(this.button7);
+            this.groupBox3.Location = new System.Drawing.Point(586, 282);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(516, 183);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "扬声器";
+            this.groupBox3.Enter += new System.EventHandler(this.groupBox3_Enter);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(289, 128);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(162, 28);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(318, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 30);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "待检测";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(22, 128);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(186, 33);
+            this.button8.TabIndex = 1;
+            this.button8.Text = "设置扬声器音量";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(22, 41);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(186, 34);
+            this.button7.TabIndex = 0;
+            this.button7.Text = "获取扬声器音量";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1114, 477);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer1;
+    }
+}
+
---
+++ Desktop/Windows程序设计/230414/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge;
+using AForge.Controls;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using VisioForge.Libs.NAudio.CoreAudioApi;
+//using VisioForge.Shared.NAudio.CoreAudioApi;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+namespace _230414
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            init();
+        }
+
+        // 初始化
+        public void init()
+        {
+            button1.Enabled = true;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = true;
+            button6.Enabled = true;
+            button7.Enabled = true;
+            button8.Enabled = true;
+            comboBox1.Items.Clear();
+            label1.Text = "待检测";
+            label2.Text = "待检测";
+            textBox2.Text = "";
+            textBox1.Text = "";
+            isVideo = false;
+            comboBox1.Text = "";
+        }
+
+        // 设置摄像头参数
+        private FilterInfoCollection videoDevices;      // 采集所有摄像头
+        public bool isVideo = false;
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+        
+        // “扫描”按键代码
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!isVideo)
+            {
+                //枚举视频输入设备
+                videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+                if (videoDevices.Count == 0)
+                {
+                    comboBox1.Text = "没有检测到视频设备！";
+                    videoDevices = null;
+                }
+                foreach (FilterInfo device in videoDevices)
+                {
+                    comboBox1.Text = device.Name;
+                    isVideo = true;
+                }
+                comboBox1.SelectedIndex = -1;
+                if (isVideo)
+                    button2.Enabled = true;
+            }          
+            button1.Enabled = false;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // “拍照”按键代码
+                Bitmap img = videoSourcePlayer1.GetCurrentVideoFrame();//拍摄
+            img.Save(string.Format("D://img.jpg"));
+               MessageBox.Show("图片保存成功！");
+            //img.Dispose();
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // “连接”按键代码
+                VideoCaptureDevice videoCapture = new VideoCaptureDevice(videoDevices[comboBox1.SelectedIndex+1].MonikerString);
+                videoSourcePlayer1.VideoSource = videoCapture;
+                videoSourcePlayer1.Start();
+                button2.Enabled = false;
+                button3.Enabled = true;
+                button4.Enabled = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // 设置拍照关闭代码
+            videoSourcePlayer1.SignalToStop();
+            videoSourcePlayer1.WaitForStop();
+                button2.Enabled = true;
+                button3.Enabled = false;
+                button4.Enabled = false;
+            
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // “获取麦克风音”按键代码
+                label1.Text = Convert.ToString(GetCurrentMicVolume());
+        }
+
+
+        private int GetCurrentMicVolume()
+        {
+            int volume = 0;
+            var enumerator = new MMDeviceEnumerator();
+
+            //获取音频输入设备
+            IEnumerable<MMDevice> captureDevices = enumerator.EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.Active).ToArray();
+            if (captureDevices.Count() > 0)
+            {
+                MMDevice mMDevice = captureDevices.ToList()[0];
+                volume = (int)(mMDevice.AudioEndpointVolume.MasterVolumeLevelScalar * 100);
+            }
+            return volume;
+        }
+
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            // “设置麦克风音”按键代码
+                string ret = "";
+                try
+                {
+                    int volnum = Convert.ToInt16(textBox1.Text);
+                    if (volnum > 100)
+                        ret = "音量不能超过100";
+                    else
+                        ret = SetCurrentMicVolume(volnum);
+                }
+                catch { ret = "请输入合法数值!"; }
+            
+        }
+        private string SetCurrentMicVolume(int volume)
+        {
+            var enumerator = new MMDeviceEnumerator();
+            IEnumerable<MMDevice> captureDevices = enumerator.EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.Active).ToArray();
+            if (captureDevices.Count() > 0)
+            {
+                MMDevice mMDevice = captureDevices.ToList()[0];
+                mMDevice.AudioEndpointVolume.MasterVolumeLevelScalar = volume / 100.0f;
+            }
+            return volume.ToString();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            // “获取扬声器音”按键代码
+            
+                label2.Text = Convert.ToString( GetCurrentSpeakerVolume());
+         }
+        private int GetCurrentSpeakerVolume()
+        {
+            int volume = 0;
+            var enumerator = new MMDeviceEnumerator();
+
+            //获取音频输出设备
+            IEnumerable<MMDevice> speakDevices = enumerator.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active).ToArray();
+            if (speakDevices.Count() > 0)
+            {
+                MMDevice mMDevice = speakDevices.ToList()[0];
+                volume = Convert.ToInt16(mMDevice.AudioEndpointVolume.MasterVolumeLevelScalar * 100);
+            }
+            return volume;
+        }
+
+        private string SetCurrentSpeakerVolume(int volume)
+            {
+                string ret = "";
+                var enumerator = new MMDeviceEnumerator();
+                IEnumerable<MMDevice> speakDevices = enumerator.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active).ToArray();
+                if (speakDevices.Count() > 0)
+                {
+
+                    MMDevice mMDevice = speakDevices.ToList()[0];
+                    mMDevice.AudioEndpointVolume.MasterVolumeLevelScalar = volume / 100.0f;
+                    ret = "设置扬声器音量为[" + volume + "]成功！";
+                }
+                else
+                    ret = "未检测到扬声器";
+                return ret;
+            }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            // “设置扬声器音”按键代码
+           string ret = "";
+               
+           int volnum = Convert.ToInt16(textBox2.Text);
+           if (volnum > 100)
+              ret = "音量不能超过100";
+           else
+              ret = SetCurrentSpeakerVolume(volnum);
+        }
+
+        private void videoSourcePlayer1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
